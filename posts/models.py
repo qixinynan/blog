7d84b45f9f9cd1ca6cb33bbb260b1404580bc3ff
Qixiny <3185835784@qq.com>
2022-12-31 19:38:28 +0800

    ready for delete markdwon

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
-from mdeditor.fields import MDTextField
+from tinymce.models import HTMLField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=64, verbose_name='文章标题')
     description = models.TextField(blank=True, verbose_name='文章描述')
     keywords = models.CharField(max_length=255, verbose_name='关键字', help_text='多个关键词使用","分割')
-    content = MDTextField(verbose_name='内容')
+    content = HTMLField(verbose_name="内容")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     views = models.PositiveIntegerField(default=0, verbose_name='阅读量')
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name='发布日期')
