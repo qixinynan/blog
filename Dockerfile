04f99067ce961e55533118a6d1fc18aee0b8d296
Qixiny <3185835784@qq.com>
2023-01-08 21:17:10 +0800

    v0.0

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM python:3.9-alpine
+FROM python:3.9
 ENV PYTHONUNBUFFERED 1
 
 RUN mkdir /app
 WORKDIR /app
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+# 安装Python依赖
+RUN pip3 install -r requirements.txt
 COPY . .
---
+++ init.sh
@@ -0,0 +1,14 @@
+echo INIT SERVER WITH DEBUG="$DEBUG"
+if [ "$DEBUG" = "on" ]
+then
+    echo STARTING DEBUG MODE
+  python3 manage.py makemigrations
+  python3 manage.py migrate
+  python3 manage.py runserver 0.0.0.0:8000
+else
+  echo STARTING PRODUCT MODE
+  python3 manage.py collectstatic --noinput
+  python3 manage.py makemigrations
+  python3 manage.py migrate
+  uwsgi --ini uwsgi.ini
+fi
